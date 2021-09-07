6ab30f0b27b5aa6d750ed7c3e303d899ed0b9849
lin <2712802058@qq.com>
2021-09-07 23:10:47 +0800

    查询 全部语句

--- src/MybatisTest.java
+++ src/MybatisTest.java
@@ -1,4 +1,5 @@
 import com.lin.entity.Person;
+import com.lin.entity.PersonDTO;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -8,8 +9,16 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+/*
+mybatis执行流程：1.导包 2.连接数据库 3.sqlSessionFactory调用sqlSession 4.sqlession可以调用对象
+ */
 public class MybatisTest {
     private SqlSession sqlSession;
     @Before //在进行test注解之前执行的方法 提取重复代码的
@@ -23,7 +32,7 @@ public class MybatisTest {
         System.out.println("sqlSession = " + sqlSession);
     }
 
-    //全查
+    //全查 无参数
     @Test
     public void test01(){
         List<Person> personList = sqlSession.selectList("com.lin.dao.PersonDao.selectAll");
@@ -32,4 +41,138 @@ public class MybatisTest {
         }
         sqlSession.close();
     }
-}
+
+    //单查 性别查询 带参数
+    @Test
+    public void test02(){
+        List<Person> personList = sqlSession.selectList("com.lin.dao.PersonDao.selectBySex", 2);
+        for (Person person : personList) {
+            System.out.println("person = " + person);
+        }
+         sqlSession.close();
+        }
+        
+    //查总条数 主要学习返回值类型 无参数
+    @Test
+    public void test03(){
+        //selectOne返回值是一条数据
+        long o = sqlSession.selectOne("com.lin.dao.PersonDao.selectCount");
+        System.out.println("o = " + o);
+        sqlSession.close();
+    }
+
+    //查询女生总条数并且分数>100
+    @Test
+    public void test04(){
+        Person person = new Person();
+        person.setScore(100);
+        person.setGender(2);
+        Object o = sqlSession.selectOne("com.lin.dao.PersonDao.selectByss01", person);
+        System.out.println("o = " + o);
+        sqlSession.close();
+    }
+
+    ////查询女生且生日>2020-09-07的人 (模拟多表 参数是map) map传参
+    @Test
+    public void test05() throws ParseException {
+        String date="2020-10-14";
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
+        Date birthday=sdf.parse(date);
+        Map map=new HashMap();
+        map.put("gender",2); //key要和值一致\
+        map.put("birthday",birthday);
+        List<Person> personList = sqlSession.selectList("com.lin.dao.PersonDao.selectBysb01", map);
+        for (Person person : personList) {
+            System.out.println("person = " + person);
+        }
+        sqlSession.close();
+    }
+
+    //查询 分值最高的人是谁 子查询
+    @Test
+    public void test06(){
+        List<Person> personList = sqlSession.selectList("com.lin.dao.PersonDao.selectBys");
+        for (Person person : personList) {
+            System.out.println("person = " + person);
+        }
+        sqlSession.close();
+    }
+
+    //所有女生男生的平均分值是多少 分组查询
+    @Test
+    public void test07(){
+        List<PersonDTO> personDTOS = sqlSession.selectList("com.lin.dao.PersonDao.selectByavg");
+        for (PersonDTO personDTO : personDTOS) {
+            System.out.println("personDTO = " + personDTO);
+        }
+        sqlSession.close();
+    }
+
+    //所有女生男生的平均分值是多少并且分数>200 有参数分数查询
+    @Test
+    public void test08(){
+        List<PersonDTO> personDTOS = sqlSession.selectList("com.lin.dao.PersonDao.selectByavg02", 200);
+        for (PersonDTO personDTO : personDTOS) {
+            System.out.println("personDTO = " + personDTO);
+        }
+        sqlSession.close();
+    }
+
+    //所有女生男生的平均分值是多少并且分数>200 有参数分数查询 map
+    @Test
+    public void test09(){
+        List<Map> maps = sqlSession.selectList("com.lin.dao.PersonDao.selectByavg03", 200);
+        for (Map map : maps) {
+            System.out.println("map = " + map);
+        }
+        sqlSession.close();
+    }
+
+    //查询姓孙的 模糊查询 $ xxx
+    @Test
+    public void test10(){
+        //There is no getter for property named 'name' in 'class java.lang.String' 因为$是拼接的 getter没有这个概念 #有
+      //  List<Person> personList = sqlSession.selectList("com.lin.dao.PersonDao.selectByName", "孙");
+        Map map=new HashMap();
+        map.put("name","孙");
+        List<Person> personList = sqlSession.selectList("com.lin.dao.PersonDao.selectByName", map);
+        for (Person person : personList) {
+            System.out.println("person = " + person);
+        }
+        sqlSession.close();
+    }
+
+    @Test
+    public void test11(){
+        List<Person> personList = sqlSession.selectList("com.lin.dao.PersonDao.selectByName02", "孙");
+        for (Person person : personList) {
+            System.out.println("person = " + person);
+        }
+        sqlSession.close();
+    }
+
+    @Test
+    public void test12(){
+        List<Person> personList = sqlSession.selectList("com.lin.dao.PersonDao.selectByName03", "孙");
+        for (Person person : personList) {
+            System.out.println("person = " + person);
+        }
+        sqlSession.close();
+    }
+
+    @Test
+    public void test13(){
+        Person person = new Person();
+        person.setName("余庆伟");
+        person.setGender(1);
+        person.setBirthday(new Date());
+        person.setAddress("湖北宜昌");
+        person.setScore(123);
+        int insert = sqlSession.insert("com.lin.dao.PersonDao.insertPerson", person);
+        System.out.println("insert = " + insert);
+        //增删改事务要提交
+        sqlSession.commit();
+        sqlSession.close();
+    }
+    }
+
--- src/com/lin/dao/PersonDao.java
+++ src/com/lin/dao/PersonDao.java
@@ -1,10 +1,40 @@
 package com.lin.dao;
 
 import com.lin.entity.Person;
+import com.lin.entity.PersonDTO;
 
 import java.util.List;
+import java.util.Map;
 
 public interface PersonDao {
+    //单表查询
     //全查
      List<Person> selectAll();
+     //单查 性别查询 参数就是where后边的值
+    List<Person> selectBySex(int sex);
+    //查总条数 主查返回类型
+    long selectCount();
+    //查询女生总条数并且分数>100（单表 所以参数是person 实体类传参）
+    //当前端传来多个参数 如果是同一个类中的属性（单表） 直接可以用这个类做参数
+    //如果不是同一个类（多表） 可以手写一个参数的实体类（DTO）或者使用map做参数 map可以接收任何数据
+    long selectByss01(Person person);
+    //查询女生且生日<2020-09-07的人 (模拟多表 参数是map)
+    List<Person> selectBysb01(Map map);
+    //查询 分值最高的人是谁 子查询
+    List<Person> selectBys();
+    //所有女生男生的平均分值是多少 分组查询 实体类
+    List<PersonDTO> selectByavg();
+    //所有女生男生的平均分值是多少并且分数>200 有参数分数查询
+    List<PersonDTO> selectByavg02(int score);
+    //所有女生男生的平均分值是多少并且分数>200 有参数分数查询 map
+    List<Map> selectByavg03(int score);
+    //查询姓孙的 模糊查询 $不建议
+    List<Person> selectByName(String name);
+    //查询姓孙的 模糊查询 concat
+    List<Person> selectByName02(String name);
+    //查询姓孙的 模糊查询
+    List<Person> selectByName03(String name);
+
+    //增加
+    int insertPerson(Person person);
 }
